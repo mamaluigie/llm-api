a1cb80a316ea9d156f134e7143c4f5d9ec01541e
mamaluigie <kane.hollingsworth@yahoo.com>
2025-10-06 17:13:55 -0500

    Added some additional organization to the file structure of the project and started on the tests. tests are incomplete at the moment.

---
+++ src/llm_api_server.py
@@ -0,0 +1,127 @@
+import random
+import argparse
+import transformers
+from huggingface_hub import login
+import json
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+def generate_response(sentence):
+
+    api_key = ""
+    # Open the config json
+    with open("credentials.json", "r") as file:
+        api_key = json.load(file)["api_key"]
+
+    # Logging into hugging face
+    login(token=api_key)
+
+    model_id = "meta-llama/Llama-3.2-1B"
+
+    pipe = transformers.pipeline(
+            "text-generation",
+            model=model_id,
+            dtype="float16",
+            device_map="auto")
+    print(pipe.model.dtype)
+    print(pipe.model.hf_device_map)
+
+    outputs = pipe(
+            sentence
+            )
+
+    return outputs
+
+
+def context_file_exists(context_id):
+    if f'chat_{context_id}' in os.listdir("./chats"):
+        return True
+    return False
+
+# ----------------------api information---------------------------
+
+class Model_Info(BaseModel):
+    context_id: Optional[str] = None
+    sentence: str
+
+
+app = FastAPI()
+
+# Helper api endpoint for checking the existance of a context
+# Including the context_id as a query parameter
+@app.get("/context_exists/")
+def context_exists(context_id: str):
+    if os.path.exists(f'./chats/chat_{context_id}'):
+        return True
+    else:
+        return False
+
+# Main api location for utilizing
+@app.post("/")
+def root(model_info: Model_Info):
+
+    sentence = model_info.sentence
+
+    # If there is no context id create one and a new chat file/directory
+    if model_info.context_id == None:
+
+        if not os.path.exists("./chats"):
+            os.mkdir("./chats")
+
+        # Generate a new context id file that already doesnt exist
+        new_context_id = random.randint(1, 999999999)
+        while context_file_exists(new_context_id):
+            new_context_id = random.randint(1, 999999999)
+
+        new_context_file = f'chat_{new_context_id}'
+
+        # Create a file with the context id in the name in the directory
+        with open(f'./chats/{new_context_file}', 'w') as context_file:
+            # Write the sentence and create a new line.
+            context_file.write(f'Q: {sentence}\n')
+
+            llm_response = generate_response(model_info.sentence)
+            context_file.write(f'A: {llm_response[0]["generated_text"]}\n')
+
+        return {"context_id": new_context_id,
+                "answer" : llm_response[0]["generated_text"]}
+
+    else:
+        # Check to see if that context_id exists in the chats folder
+        if not os.path.exists("./chats"):
+            return {"code":"Error: No chats directory exists. Initialize with a context id"}
+
+        elif context_file_exists(model_info.context_id):
+
+            context_file_name = f'chat_{model_info.context_id}'
+            print(context_file_name)
+
+            with open(f'./chats/{context_file_name}', 'r') as context_file:
+
+                full_context = context_file.read()
+
+                # Add the new question to the full_context
+                full_context = f'This is the previous chat:\n{full_context}\n\n'
+
+                # now add the new question
+                full_context = full_context + f'Now answer this question:\n\n{sentence}\n\nDo not repeat any of the previous chat in the answer.'
+
+            print(full_context)
+            with open(f'./chats/{context_file_name}', 'a') as context_file:
+
+                llm_response = generate_response(full_context)
+                llm_answer = llm_response[0]["generated_text"]
+
+                # Write the question and response to the context file
+                context_file.write(f'Q: {sentence}\n')
+                context_file.write(f'A: {llm_answer}\n')
+
+            return {"context_id": model_info.context_id,
+                    "answer" : llm_answer}
+
+        else:
+            return {"code":"Error: Context does not exists for that context id."}
+
+
